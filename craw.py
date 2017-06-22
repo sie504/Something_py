47aebc7187d32c28504add8d079de4a020418f8a
sie504 <python2017@sina.com>
2017-06-22 16:09:23 +0800

    some change

--- craw.py
+++ craw.py
@@ -6,6 +6,7 @@ import urllib2
 import threading
 import Queue
 import time
+import random
 
 q = Queue.Queue()     #FIFO 先进先出
 threading_num = 10
@@ -15,21 +16,36 @@ Baidu_spider = "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/
 #User-Agent
 exclude_list = ['.jpg','.gif','.css','.png','.js']     #过滤排除列表
 
-f = open('/root/show_me_code/XTeach/urlfuzzing/CMS/wordpress.txt')
-lines = f.readlines()
-f.close()
+"""
+代理服务
+proxy_list = [
+    {'http':'219.232.125.232:3128'},
+    {'http':'124.88.31.80'},
+    {'http':'113.69.252:9000'},
+]"""
 
-for line in lines:
-    line = line.rstrip()
-    if os.path.splitext(line)[1] not in exclude_list:
-    #判断后缀是否是jpg,gif,不是的就输出
-        q.put(line)      #将line传入队列
+
+#f = open('/root/show_me_code/XTeach/urlfuzzing/CMS/wordpress.txt')
+#lines = f.readlines()
+#f.close()
+with open('/root/show_me_code/XTeach/urlfuzzing/CMS/wordpress.txt','r') as lines:
+
+    for line in lines:
+        line = line.rstrip()
+        if os.path.splitext(line)[1] not in exclude_list:
+         #判断后缀是否是jpg,gif,不是的就输出
+            q.put(line)      #将line传入队列
 
 def crawler():
     while not q.empty():      #只要队列不为空
         path = q.get()
         url = "%s%s" % (domain_name,path)
-        
+        #random_proxy = random.choice(proxy_list)
+        #proxy_support = urllib2.ProxyHandler(random_proxy)    #使用代理
+        #opener = urllib2.build_opener(proxy_support)   #开启
+        #urllib2.install_opener(opener)    #安装
+
+
         headers = {}
         headers['User-Agent'] = Baidu_spider
         request = urllib2.Request(url,headers=headers)
@@ -42,7 +58,7 @@ def crawler():
                 print "Status [%s]  -path: %s" %(response.code,path)
         
             response.close()
-            time.sleep()
+            time.sleep(4)
         except urllib2.HTTPError as e:
             print e.code, path
             #pass
